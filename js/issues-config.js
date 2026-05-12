4c6a076a3e48c43c7051d2b5f795b8df4363a70d
extendedwhanau <max@extendedwhanau.com>
2026-05-13 09:28:26 +1200

    ddd

--- js/issues-config.js
+++ js/issues-config.js
@@ -1,6 +1,10 @@
 /**
  * Marinade — issue content & assets
  * ---------------------------------
+ * Issue 01 (Writing) cover letters are not pasted here: they live as plain SVG
+ * files in svg/issue-01/ (letter-m.svg … letter-e.svg). letter-a.svg is reused
+ * for both “A” glyphs. Change fill inside those files to retint the cover.
+ *
  * When you add Issue 04: duplicate a tab + pane in index.html (copy the closest
  * interaction pattern), then add a new entry here and wire data-w-tab / classes.
  *
@@ -10,14 +14,14 @@ window.MARINADE_ISSUES = {
   writing: {
     tabKey: "Writing",
     contributors:
-      "\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0Daniel Michael Satele, Pandora Fulimalo Pereira, Ioana Gordon-Smith, Lagi-Maama Academy & Consultancy, Lana Lopesi, Leone Samu Tui, Luisa Keteiyau Tora, Natasha Matila-Smith, Ngahuia Harrison, Sean Mallon, Stephanie Oberg, Peter Brunt, Raymond Sagapolutele, Talei Tuʻinukuafe, Teresia Teaiwa",
+      "Daniel Michael Satele, Pandora Fulimalo Pereira, Ioana Gordon-Smith, Lagi-Maama Academy & Consultancy, Lana Lopesi, Leone Samu Tui, Luisa Keteiyau Tora, Natasha Matila-Smith, Ngahuia Harrison, Sean Mallon, Stephanie Oberg, Peter Brunt, Raymond Sagapolutele, Talei Tuʻinukuafe, Teresia Teaiwa",
     buyUrl:
       "https://shop.moanafresh.com/products/marinade-aotearoa-journal-of-moana-art",
   },
   international: {
     tabKey: "International",
     contributors:
-      "\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0Ahilapalapa Rands, Andrea Low, Christina Pataialii, Cora-Allan, Ioana Gordon-Smith, Janet Lilo, Jim Vivieaere, Kalisolaite ‘Uhila, Lana Lopesi, Leafā Wilson/Olga Krause, Léuli Eshrāghi, Misal Adnan Yildiz, Natasha Matila-Smith, Tuāfale Tanoaʻi",
+      "Ahilapalapa Rands, Andrea Low, Christina Pataialii, Cora-Allan, Ioana Gordon-Smith, Janet Lilo, Jim Vivieaere, Kalisolaite ‘Uhila, Lana Lopesi, Leafā Wilson/Olga Krause, Léuli Eshrāghi, Misal Adnan Yildiz, Natasha Matila-Smith, Tuāfale Tanoaʻi",
     buyUrl:
       "https://shop.moanafresh.com/collections/books-1/products/book-marinade-aotearoa-journal-of-moana-art-issue-02",
     stutterImage: "images/img_1.svg",
@@ -25,7 +29,7 @@ window.MARINADE_ISSUES = {
   friendship: {
     tabKey: "Friendship",
     contributors:
-      "\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0Edith Amituanai, Ralph Brown, Peter Brunt, Kiri Chan, Feeonaa Clifton, Ioana Gordon-Smith, Jacki Leota-Mua, Lindah Lepou, Lana Lopesi, Andrea Low, Gloriana Meyers, Rosanna Raymond, Tuāfale Tanoaʻi, Seutaʻafili Patrick Thomsen, Luisa Keteiyau Tora, Manuhaʻapai Vaeatangitau, John Vea",
+      "Edith Amituanai, Ralph Brown, Peter Brunt, Kiri Chan, Feeonaa Clifton, Ioana Gordon-Smith, Jacki Leota-Mua, Lindah Lepou, Lana Lopesi, Andrea Low, Gloriana Meyers, Rosanna Raymond, Tuāfale Tanoaʻi, Seutaʻafili Patrick Thomsen, Luisa Keteiyau Tora, Manuhaʻapai Vaeatangitau, John Vea",
     buyUrl:
       "https://shop.moanafresh.com/products/marinade-aotearoa-journal-of-moana-art-issue-02-copy",
     comparison: {
